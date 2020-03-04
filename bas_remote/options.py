7a882c235057d0f5a135c233bec525777dd8d369
CheshireCaat <likangt2012@gmail.com>
2020-03-04 19:17:03 +0300

    Update
    Remove callback class
    Update options

--- bas_remote/options.py
+++ bas_remote/options.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
+from os.path import abspath, join
 from os import getcwd
-from os import path
 
 
 @dataclass
@@ -8,24 +8,30 @@ class Options:
     """Class that contains client settings.
 
     Attributes:
-        workingDir (str): Location of the selected working folder.
-        scriptName (str): Name of the selected private script.
-        password (str): Password from a user account with access to the script.
-        login (str): Login from a user account with access to the script.
+        working_dir (str):
+            Location of the selected working folder.
+        script_name (str):
+            Name of the selected private script.
+        password (str):
+            Password from a user account with access to the script.
+        login (str):
+            Login from a user account with access to the script.
     """
 
-    workingDir: str = path.join(getcwd(), 'data')
+    working_dir: str = join(getcwd(), 'data')
 
-    scriptName: str = ''
+    script_name: str = ''
 
     password: str = ''
 
     login: str = ''
 
     def __post_init__(self):
-        if not self.workingDir:
-            msg = "Field 'workingDir' must be specified"
-            raise ValueError(msg)
-        if not self.scriptName:
-            msg = "Field 'scriptName' must be specified"
-            raise ValueError(msg)
+        if not self.working_dir:
+            raise ValueError("Field 'workingDir' must be specified")
+        if not self.script_name:
+            raise ValueError("Field 'scriptName' must be specified")
+        self.working_dir = abspath(self.working_dir)
+
+
+__all__ = ['Options']
