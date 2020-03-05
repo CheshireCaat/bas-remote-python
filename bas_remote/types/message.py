5e1148406de95fd79333b35bb598dd0d0cbb1e32
CheshireCaat <likangt2012@gmail.com>
2020-03-05 16:02:38 +0300

    Update types
    Add more docs

--- bas_remote/types/message.py
+++ bas_remote/types/message.py
@@ -8,22 +8,31 @@ from dataclasses_json import dataclass_json, config
 @dataclass_json
 @dataclass
 class Message:
+    """Class that represents default BAS message."""
+    
     async_: bool = field(
         metadata=config(
             field_name="async",
         )
     )
+    """Is message async."""
+
     type_: str = field(
         metadata=config(
             field_name="type")
     )
+    """Message type string."""
+
     id_: int = field(
         metadata=config(
             field_name="id"
         ),
         default=randint(100000, 999999)
     )
+    """Message id number."""
+
     data: Any = None
+    """Message data object."""
 
 
 __all__ = ['Message']
--- bas_remote/types/response.py
+++ bas_remote/types/response.py
@@ -7,9 +7,16 @@ from dataclasses_json import dataclass_json, LetterCase
 @dataclass_json(letter_case=LetterCase.PASCAL)
 @dataclass
 class Response:
+    """Class that represents default BAS response."""
+
     success: bool = False
+    """[description]"""
+
     message: str = None
+    """Response error message."""
+
     result: Any = None
+    """Response result object."""
 
 
 __all__ = ['Response']
