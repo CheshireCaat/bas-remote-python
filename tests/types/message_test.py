effe45e1a1938f1250c3d576e3d433b9f0bde326
CheshireCaat <likangt2012@gmail.com>
2020-03-06 14:58:34 +0300

    Tests update
    Update messages test (from/to dict)
    Update response test (from/to dict)

--- tests/types/message_test.py
+++ tests/types/message_test.py
@@ -4,14 +4,26 @@ from bas_remote.types.message import Message
 
 
 class MessageTestCase(unittest.TestCase):
+    test_data = [
+        (Message(id_=1, async_=True, type_='log', data='Hello'),
+         {"async": True, "type": "log", "id": 1, "data": "Hello"}),
+        (Message(id_=1, async_=False, type_='log', data='Hello'),
+         {"async": False, "type": "log", "id": 1, "data": "Hello"})
+    ]
 
-    def test_message_from_json(self):
-        msg = Message(id=1, async_=True, type_='log', data='Hello')
-        self.assertEqual(Message.from_json('{"async": true, "type": "log", "id": 1, "data": "Hello"}'), msg)
+    def test_message_from_dict(self):
+        for i in range(len(self.test_data)):
+            with self.subTest(i=i):
+                message = self.test_data[i][0]
+                dict_ = self.test_data[i][1]
+                self.assertEqual(Message.from_dict(dict_), message)
 
-    def test_message_to_json(self):
-        msg = Message(id=1, async_=True, type_='log', data='Hello')
-        self.assertEqual(msg.to_json(), '{"async": true, "type": "log", "id": 1, "data": "Hello"}')
+    def test_message_to_dict(self):
+        for i in range(len(self.test_data)):
+            with self.subTest(i=i):
+                message = self.test_data[i][0]
+                dict_ = self.test_data[i][1]
+                self.assertEqual(message.to_dict(), dict_)
 
 
 if __name__ == '__main__':
--- tests/types/response_test.py
+++ tests/types/response_test.py
@@ -4,14 +4,26 @@ from bas_remote.types.response import Response
 
 
 class ResponseTestCase(unittest.TestCase):
+    test_data = [
+        (Response(message='Hello', success=True, result=None),
+         {"Success": True, "Message": "Hello", "Result": None}),
+        (Response(message='Hello', success=False, result=None),
+         {"Success": False, "Message": "Hello", "Result": None})
+    ]
 
-    def test_response_from_json(self):
-        resp = Response(message='Hello', success=True, result=None)
-        self.assertEqual(Response.from_json('{"Success": true, "Message": "Hello", "Result": null}'), resp)
+    def test_response_from_dict(self):
+        for i in range(len(self.test_data)):
+            with self.subTest(i=i):
+                response = self.test_data[i][0]
+                dict_ = self.test_data[i][1]
+                self.assertEqual(Response.from_dict(dict_), response)
 
-    def test_response_to_json(self):
-        resp = Response(message='Hello', success=True, result=None)
-        self.assertEqual(resp.to_json(), '{"Success": true, "Message": "Hello", "Result": null}')
+    def test_response_to_dict(self):
+        for i in range(len(self.test_data)):
+            with self.subTest(i=i):
+                response = self.test_data[i][0]
+                dict_ = self.test_data[i][1]
+                self.assertEqual(response.to_dict(), dict_)
 
 
 if __name__ == '__main__':
