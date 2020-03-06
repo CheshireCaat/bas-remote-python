b9dc47789d45b115f9dc1d182da965daab4b65a2
CheshireCaat <likangt2012@gmail.com>
2020-03-06 19:59:05 +0300

    Services update

--- bas_remote/services/engine_service.py
+++ bas_remote/services/engine_service.py
@@ -1,62 +1,70 @@
-import aiofiles
-import aiohttp
 import asyncio
-import shutil
-import os
-
-from os import path
-from zipfile import ZipFile
-from subprocess import Popen
+import subprocess
+from os import listdir, makedirs, path
 from platform import machine
+from shutil import rmtree
+from zipfile import ZipFile
+
+from aiofiles import open
+from aiohttp import ClientSession
 from filelock import FileLock, Timeout
-from bas_remote.errors import ScriptNotSupportedError
-from bas_remote.errors import ScriptNotExistError
-from bas_remote.objects import Script
-from .base_service import BaseService
 
-
-__all__ = ['EngineService']
+from bas_remote.errors import ScriptNotExistError, ScriptNotSupportedError
+from bas_remote.types import Script
 
 ENDPOINT = 'https://bablosoft.com'
 
 
-class EngineService(BaseService):
+class EngineService:
     """Service that provides methods for interacting with BAS engine."""
 
-    def __init__(self, client):
-        super().__init__(client)
-        self.scriptName = client._options.scriptName
-        self.workingDir = client._options.workingDir
+    _exe_dir: str = None
+    """The path to the directory in which the executable file of the engine is located."""
 
-        self.scriptDir = path.join(self.workingDir, "run", self.scriptName)
-        self.engineDir = path.join(self.workingDir, "engine")
+    _zip_dir: str = None
+    """The path to the directory in which the archive file of the engine is located."""
+
+    def __init__(self, client):
+        """Create an instance of EngineService class."""
+        script_name = client.options.script_name
+        working_dir = client.options.working_dir
+        self._loop = client.loop
+
+        self._script_dir = path.join(working_dir, "run", script_name)
+        self._engine_dir = path.join(working_dir, "engine")
+        self._script_name = script_name
+
+        self._process = None
+        self._lock = None
 
     async def start(self, port: int) -> None:
         """Asynchronously start the engine service with the specified port.
 
-        Args:
-            port (int): Selected port number.
+        Arguments:
+            port (int):
+                Selected port number.
         """
+        arch = 64 if machine().endswith('64') else 32
+        zip_name = f'FastExecuteScriptProtected.x{arch}'
+        url_name = f'FastExecuteScriptProtected{arch}'
 
-        zipName, urlName = get_names()
+        zip_path = path.join(self._zip_dir, f'{zip_name}.zip')
 
-        zipPath = path.join(self.zipDir, f'{zipName}.zip')
+        if not path.exists(self._zip_dir):
+            makedirs(self._zip_dir)
+            await self._download_executable(zip_path, zip_name, url_name)
 
-        if not path.exists(self.zipDir):
-            os.makedirs(self.zipDir)
-            await self._download(zipPath, zipName, urlName)
-
-        if not path.exists(self.exeDir):
-            os.makedirs(self.exeDir)
-            await self._extract(zipPath)
+        if not path.exists(self._exe_dir):
+            makedirs(self._exe_dir)
+            await self._extract_executable(zip_path)
 
         self._start_engine_process(port)
         self._clear_run_directory()
 
     async def initialize(self):
-        url = f'{ENDPOINT}/scripts/{self.scriptName}/properties'
+        url = f'{ENDPOINT}/scripts/{self._script_name}/properties'
 
-        async with aiohttp.ClientSession(loop=self._loop) as session:
+        async with ClientSession(loop=self._loop) as session:
             async with session.get(url) as response:
                 script = Script(await response.json())
 
@@ -66,15 +74,15 @@ class EngineService(BaseService):
         if not script.is_supported:
             raise ScriptNotSupportedError()
 
-        self.zipDir = path.join(self.engineDir, script.engine_version)
-        self.exeDir = path.join(self.scriptDir, script.hash[0:5])
+        self._zip_dir = path.join(self._engine_dir, script.engine_version)
+        self._exe_dir = path.join(self._script_dir, script.hash[0:5])
 
-    async def _download(self, zipPath: str, zipName: str, urlName: str) -> None:
-        url = f'{ENDPOINT}/distr/{urlName}/{path.basename(self.zipDir)}/{zipName}.zip'
+    async def _download_executable(self, zip_path: str, zip_name: str, url_name: str) -> None:
+        url = f"{ENDPOINT}/distr/{url_name}/{path.basename(self._zip_dir)}/{zip_name}.zip"
 
-        async with aiohttp.ClientSession(loop=self._loop) as session:
+        async with ClientSession(loop=self._loop) as session:
             async with session.get(url) as response:
-                async with aiofiles.open(zipPath, 'wb') as file:
+                async with open(zip_path, 'wb') as file:
                     while True:
                         chunk = await response.content.read(1024 * 16)
                         if not chunk:
@@ -82,52 +90,48 @@ class EngineService(BaseService):
                         await file.write(chunk)
                     return await response.release()
 
-    async def _extract(self, zipPath: str) -> None:
-        # TODO: aiozipstream
-        with ZipFile(zipPath, 'r') as file:
-            async def coro(member): 
-                file.extract(member, self.exeDir, None)
+    async def _extract_executable(self, zip_path: str) -> None:
+        with ZipFile(zip_path, 'r') as file:
+            async def task(name, zip_file: ZipFile):
+                zip_file.extract(name, self._exe_dir, None)
 
-            tasks = [self._loop.create_task(coro(member))
-                     for member in file.namelist()]
-            await self._loop.wait(tasks)
+            tasks = [task(name, file) for name in file.namelist()]
+            await asyncio.wait(tasks)
 
-    def _start_engine_process(self, port: int):
-        self._process = Popen(
-            [
-                path.join(self.exeDir, 'FastExecuteScript.exe'),
-                f'--remote-control-port={port}',
-                f'--remote-control'
-            ],
-            cwd=self.exeDir)
-        lock = path.join(self.exeDir, '.lock')
+    def _start_engine_process(self, port: int) -> None:
+        self._process = subprocess.Popen([
+            path.join(self._exe_dir, 'FastExecuteScript.exe'),
+            f'--remote-control-port={port}',
+            f'--remote-control'
+        ], cwd=self._exe_dir)
+
+        lock = self._get_lock_path()
         self._lock = FileLock(lock)
         self._lock.acquire()
 
-    def _clear_run_directory(self):
-        for name in os.listdir(self.scriptDir):
-            name = path.join(self.scriptDir, name)
-            file = path.join(name, '.lock')
-            if not path.isfile(name):
-                if not is_locked(file):
-                    shutil.rmtree(name)
+    def _clear_run_directory(self) -> None:
+        for dir_path in [name for name in listdir(self._script_dir)
+                         if path.isfile(name)]:
+            dir_path = path.join(self._script_dir, dir_path)
+            lock_path = self._get_lock_path(dir_path)
+            if not is_locked(lock_path):
+                rmtree(dir_path)
+
+    def _get_lock_path(self, dir_path=None) -> str:
+        return path.join(dir_path or self._exe_dir, '.lock')
 
     async def close(self) -> None:
         self._process.kill()
         self._lock.release()
 
 
-def is_locked(path):
+def is_locked(lock_path):
     try:
-        with FileLock(path, timeout=0.5) as lock:
+        with FileLock(lock_path, timeout=0.5) as lock:
             with lock.acquire():
                 return False
     except Timeout:
         return True
 
 
-def get_names():
-    arch = 64 if machine().endswith('64') else 32
-    zip_name = f'FastExecuteScriptProtected.x{arch}'
-    url_name = f'FastExecuteScriptProtected{arch}'
-    return zip_name, url_name
+__all__ = ['EngineService']
--- bas_remote/services/socket_service.py
+++ bas_remote/services/socket_service.py
@@ -1,74 +1,88 @@
 import asyncio
-import json
 
-from websockets.exceptions import (ConnectionClosedError, ConnectionClosedOK)
 from websockets import WebSocketClientProtocol, connect
-from bas_remote.errors import SocketNotConnectedError
-from .base_service import BaseService
+from websockets.exceptions import (
+    ConnectionClosedError,
+    ConnectionClosedOK
+)
 
-__all__ = ['SocketService']
+from bas_remote.errors import SocketNotConnectedError
+from bas_remote.types import Message
 
 SEPARATOR = '---Message--End---'
 
 
-class SocketService(BaseService):
+class SocketService:
     """Service that provides methods for interacting with BAS socket."""
 
+    _socket: WebSocketClientProtocol = None
+
+    _buffer: str = ''
+
     def __init__(self, client):
-        super().__init__(client)
-        self._websocket = None
-        self._buffer = ''
-
-    async def _message_received(self, message) -> None:
-        buffer = (self._buffer + message).split(SEPARATOR)
-        for message in [item for item in buffer if item]:
-            unpacked = json.loads(message)
-            await self._client._on_message_received(unpacked)
-        self._buffer = buffer.pop()
-
-    async def _message_sent(self, message) -> None:
-        await self._client._on_message_sent(message)
-
-    async def _closed(self) -> None:
-        await self._client._on_socket_closed()
-        self._loop.create_task(self.close())
-
-    async def _opened(self) -> None:
-        await self._client._on_socket_opened()
-        self._loop.create_task(self.listen())
+        """Create an instance of SocketService class."""
+        self._emit = client.emit
+        self._loop = client.loop
 
     async def start(self, port: int) -> None:
         """Asynchronously start the socket service with the specified port.
 
-        Args:
+        Arguments:
             port (int): Selected port number.
         """
         attempt = 1
-        while not self._websocket or not self._websocket.open:
+        while not self.is_connected:
             try:
-                self._websocket = await connect(f'ws://127.0.0.1:{port}')
-            except:
+                self._socket = await connect(f'ws://127.0.0.1:{port}', loop=self._loop)
+            except ConnectionRefusedError:
                 if attempt == 60:
                     raise SocketNotConnectedError()
                 await asyncio.sleep(1)
                 attempt += 1
-        await self._opened()
+        self._opened()
+
+    @property
+    def is_connected(self) -> bool:
+        return self._socket is not None and self._socket.open
+
+    def _process_data(self, data: str) -> None:
+        buffer = (self._buffer + data).split(SEPARATOR)
+        for message in [item for item in buffer if item]:
+            unpacked = Message.from_json(message)
+            self._emit('message_received', unpacked)
+        self._buffer = buffer.pop()
+
+    def _closed(self) -> None:
+        """Function that is called when the connection is closed."""
+        self._emit('socket_close')
+        self._loop.create_task(self.close())
+
+    def _opened(self) -> None:
+        """Function that is called when the connection is opened."""
+        self._emit('socket_open')
+        self._loop.create_task(self.listen())
 
     async def listen(self) -> None:
         while True:
             try:
-                message = await self._websocket.recv()
-                await self._message_received(message)
+                data = await self._socket.recv()
+                self._process_data(data)
             except ConnectionClosedError:
                 break
             except ConnectionClosedOK:
                 break
-        await self._closed()
+        self._closed()
 
-    async def send(self, message: dict) -> None:
-        packet = json.dumps(message) + SEPARATOR
-        await self._websocket.send(packet)
-        await self._message_sent(message)
+    async def send(self, message: Message) -> int:
+        packet = message.to_json() + SEPARATOR
+        await self._socket.send(packet)
+        self._emit('message_sent', message)
+        return message.id_
 
     async def close(self) -> None:
-        await self._websocket.close()
+        if not self.is_connected:
+            return
+        await self._socket.close()
+
+
+__all__ = ['SocketService']
