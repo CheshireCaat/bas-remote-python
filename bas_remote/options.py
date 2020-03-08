bea70c0a71fd6400d2b128a06985ab46df81891c
CheshireCaat <likangt2012@gmail.com>
2020-03-08 16:56:15 +0300

    Update options.py
    Update docstring
    Optimize imports

--- bas_remote/options.py
+++ bas_remote/options.py
@@ -1,30 +1,22 @@
 from dataclasses import dataclass
-from os import getcwd
-from os import path
+from os import getcwd, path
 
 
 @dataclass
 class Options:
-    """Class that contains client settings.
-
-    Attributes:
-        working_dir (str):
-            Location of the selected working folder.
-        script_name (str):
-            Name of the selected private script.
-        password (str):
-            Password from a user account with access to the script.
-        login (str):
-            Login from a user account with access to the script.
-    """
+    """Class that contains client settings."""
 
     working_dir: str = path.join(getcwd(), 'data')
+    """Location of the selected working folder."""
 
     script_name: str = ''
+    """Name of the selected private script."""
 
     password: str = ''
+    """Password from a user account with access to the script."""
 
     login: str = ''
+    """Login from a user account with access to the script."""
 
     def __post_init__(self):
         if not self.working_dir:
